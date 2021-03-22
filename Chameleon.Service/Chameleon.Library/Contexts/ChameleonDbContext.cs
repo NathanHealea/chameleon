5699a6b79003ae85483498e531527f23e1e30c94
Nathan Healea <nate.healea@gmail.com>
2021-03-22 16:55:56 -0700

    CHMLN-7-Implement PostgreSQL database configuration and integration

---
+++ Chameleon.Service/Chameleon.Library/Contexts/ChameleonDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NathaHealea.Chameleon.Library.Contexts
+{
+    public class ChameleonDbContext: DbContext
+    {
+        public ChameleonDbContext(DbContextOptions<ChameleonDbContext> options) : base(options) { }
+
+        // Entitie Tables
+        
+    }
+}
