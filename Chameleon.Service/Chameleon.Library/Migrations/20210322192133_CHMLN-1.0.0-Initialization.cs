80b446a0fa419c2381a88b62b2bc565e9578bcdf
Nathan Healea <nate.healea@gmail.com>
2021-03-22 16:55:56 -0700

    CHMLN-SRVC-7-Implement PostgreSQL database configuration and integration

---
+++ Chameleon.Service/Chameleon.Library/Migrations/20210322192133_CHMLN-1.0.0-Initialization.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NathaHealea.Chameleon.Library.Migrations
+{
+    public partial class CHMLN100Initialization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
